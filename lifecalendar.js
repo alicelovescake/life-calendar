171e17e3e2993560acedf7e59c73b6a820ba5018
Alice Zhao <azhao991@gmail.com>
2020-07-13 17:28:23 -0700

    Add styling

--- lifecalendar.js
+++ lifecalendar.js
@@ -1,8 +1,11 @@
+const totalImages = 22;
+const num = Math.ceil(Math.random()* totalImages);
+document.body.background = 'images/background/'+num+'.jpg';  
 
-console.log("HI")
+
 const lifeCalendar = [];
 let prevDate = null
-//get value of birthday from stored value
+//get value of birthday from stored value (May 1, 1991)
 
 chrome.storage.sync.get(['birthdate'], ({ birthdate }) => {
     if (!birthdate) {
@@ -16,33 +19,32 @@ chrome.storage.sync.get(['birthdate'], ({ birthdate }) => {
 
         //Fill year with weeks
         for (let j = 0; j < 52; j++) {
-            prevDate = new Date(prevDate);
-            prevDate.setDate(prevDate.getDate() + 7);
-            year.push({ date: prevDate });
+            prevDate = new Date(prevDate); // (May 1, 1991 0:00:00)string into data structure turning stored birthday into a date object whose component values (year, month, day, hour minute, etc) all com from certain parameters. Any missing field is lowest ossible value
+            prevDate.setDate(prevDate.getDate() + 7);   //getDate() returns day of the month (1) setDate (1 +7)-> sets the prevDay to 8 (7 days = 1 box. 52 boxes = 1 row)
+            year.push({ date: prevDate });   //push into the year array key date, value birthday
         }
         lifeCalendar.push(year);
     }
-    const today = new Date();
-    const calendar = document.getElementById('calendar');
+    const today = new Date(); 
+    const calendar = document.getElementById('calendar');  // document method that returns element object representing element whose ID matches string. ID= calendar
 
     lifeCalendar.forEach(year => {
-        const yearRow = document.createElement('div');
-        yearRow.classList.add('year');
+        const yearRow = document.createElement('div'); //creates new div element for yearRow
+        yearRow.classList.add('year');  // creates new class called year on yearRow
         year.forEach(week => {
-            const weekCol = document.createElement('div');
+            const weekCol = document.createElement('div'); // creates  div element for weekCol
             if (week.date < today){
-                weekCol.classList.add('past');
+                weekCol.classList.add('past'); // creates new class called past on weekCol in the date less than today
             }
-            weekCol.classList.add('week');
-            yearRow.appendChild(weekCol);
+            weekCol.classList.add('week');// creates new classed called week on weekCol in the date greater than today
+            yearRow.appendChild(weekCol); //
         })
         calendar.appendChild(yearRow);
     })
 })
     //Start the date on birthday entered
 
-
-
+    
 
 
 
