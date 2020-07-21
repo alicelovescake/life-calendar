0ad4e6c7da0f85ae8b884c64af12159d04302979
Alice Zhao <azhao991@gmail.com>
2020-07-20 18:37:43 -0700

    Remove permissions that aren't needed

--- popup.js
+++ popup.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', () =>{
     const datePicker = document.querySelector('#birthdate');
 
     chrome.storage.sync.get(['birthdate'], ({ birthdate }) => {
-            datePicker.value = birthdate;
+        datePicker.value = birthdate;
     });
     datePicker.addEventListener('change', () => {
         chrome.storage.sync.set({ birthdate: datePicker.value }, () => {
